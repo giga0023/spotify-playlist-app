042510b81e6410d5eef412eb4eeede28be071080
Giga <243737693+giga0023@users.noreply.github.com>
2026-03-20 18:26:32 +0800

    feat: add App component logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,32 @@
+import { useState } from "react";
+
 function App() {
+
+  const [userSearch, setUserSearch] = useState([])
+  const [trackList, setTrackList] = useState([])
+  const [playlistName, setPlaylistName] = useState('')
+
+  const addTrackToPlaylist = (track) => (
+    setTrackList((prev) => {
+      if (!prev.some((t) => t.id === track.id)) {
+        return [track, ...prev]
+      } else {
+        return prev
+      }
+    })
+  );
+
+  const removeTrackToPlaylist = (track) => (
+    setTrackList((prev) => {
+      return prev.filter((t) => t.id !== track.id)
+    })
+  );
+
+  const handleNameChange = ({target}) => (
+    setPlaylistName(target.value)
+  )
+
+
   return (
     <div>
       <h1>Playlist Spotify App</h1>
